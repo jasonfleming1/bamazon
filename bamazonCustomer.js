af50dadd400700fa607c4bc38ff62dc223024c02
phantom <phantomdesign9@gmail.com>
2019-04-12 14:18:48 -0500

    cli table api and inquire

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,33 +1,54 @@
 //require npm packages
-var inquirer = require('inquirer');
-var mysql = require('mysql');
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var Table = require("cli-table");
+var table = new Table({
+  head: ["Item ID", "Product", "Department", "Price", "Av Qty"],
+  colWidths: [11, 40, 20, 8, 8]
+});
 
 //here we define our connection to bamazonDB
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "bamazon"
-  });
-  
-  //here we manage the connections
-  connection.connect(function(err) {
-      if (err) throw err;
-      console.log("you are connected to our store! " + connection.threadId);
-      afterConnection();
-  });
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "bamazon"
+});
 
-//here were are preseting the bamazon products to a connected user
-function afterConnection() {
-    connection.query("SELECT * FROM products", function(err, res) {
-        if (err) throw err;
-        console.log(res);
-        connection.end(); //move this after adding more features
-    });
+//here we manage the connections
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Welcome to our store!");
+  shopping();
+});
+
+//here were are preseting the bamazon products to a connected user (need to put this into a function)
+function shopping() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_qty]);
+    }
+    console.log(table.toString());
+
+    //here we will prompt the user to select a product ID and a qty
+    inquirer.prompt([
+        {
+        name: 'choice',
+        type: 'input',
+        message: 'What is the Item ID you want to buy?'
+    }, 
+    { 
+        name: 'choice',
+        type: 'input',
+        message: 'How many do you want to buy?'
+    }
+    ]);
+  });
+  connection.end();
 }
 
-//here we will prompt the user to select a product ID and a qty
 
 //here we will check to see if there is enough stock
 
